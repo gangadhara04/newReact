0dccf89004c83a5f448b83a70b4099b34dc7d582
gangadhara <your.new.email@example.com>
2023-06-16 12:53:39 +0530

    new commit

--- src/components/Addmodal/index.js
+++ src/components/Addmodal/index.js
@@ -1,36 +1,39 @@
 import React from 'react'
-import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+import { Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
 import Form from '../Form';
 
-const AddModal= (props) => {
-  const {toggleModalClose,modalTitle,modalBody,madalFotter,isOpen,toggle, onSubmit}=props
+const AddModal = (props) => {
+  const { toggleModalClose, modalTitle, modalBody, madalFotter, isOpen, toggle, initialValues, onSubmit } = props
   return (
     <div>
-       <Modal
-       isOpen={isOpen}
-      toggle={toggle}
-
-       >
-        <ModalHeader
-        toggle={toggleModalClose}
-        close={
-          <button className="close" onClick={toggleModalClose}>
-            ×
-          </button>
-        }
-        cssModule={{ "modal-title": "w-100 text-center" }}
+      <Modal
+        isOpen={isOpen}
+        toggle={toggle}
       >
-         <span className="text-center">{modalTitle}</span>
+        <ModalHeader
+          toggle={toggleModalClose}
+          close={
+            <button className="close" onClick={toggleModalClose}>
+              ×
+            </button>
+          }
+          cssModule={{ "modal-title": "w-100 text-center" }}
+        >
+          <span className="text-center">{modalTitle}</span>
         </ModalHeader>
-        <Form onSubmit={onSubmit}>
-        <ModalBody className="p-2">
-          <div className="mt-2 mb-3">{modalBody}</div>
-        </ModalBody>
+        <Form initialValues={initialValues}
+          enableReinitialize={true}
+
+          onSubmit={onSubmit}>
+          <ModalBody className="p-2">
+            <div className="mt-2 mb-3">{modalBody}</div>
+          </ModalBody>
+          <ModalFooter>
+            {madalFotter}
+          </ModalFooter>
         </Form>
-        <ModalFooter>
-          {madalFotter}
-        </ModalFooter>
-       </Modal>
+
+      </Modal>
     </div>
   )
 }
